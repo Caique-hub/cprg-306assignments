4e5551fbf9f9047ba9743395119de457ae5b9a94
Caique-hub <caiquedecastro@outlook.com.br>
2025-12-16 14:08:27 -0700

    Add Week 2 page and update root page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <section>
+      <p>Caique de Castro Correia</p>
+      <Link href="https://github.com/Caique-hub/cprg-306assignments">
+        https://github.com/Caique-hub/cprg-306assignments
+      </Link>
+    </section>
+  );
+}
